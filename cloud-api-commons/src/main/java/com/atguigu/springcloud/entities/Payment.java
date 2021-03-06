d12b2ce393eb15c5dce2bb9479bff2f1628cdc81
dnovelty <dignovelty@gmail.com>
2021-03-06 13:50:19 +0800

    feign调用

--- cloud-api-commons/src/main/java/com/atguigu/springcloud/entities/Payment.java
+++ cloud-api-commons/src/main/java/com/atguigu/springcloud/entities/Payment.java
@@ -17,4 +17,5 @@ public class Payment implements Serializable
 {
     private Long id;
     private String serial;
+    private String message;
 }
---
+++ cloud-consumer-feign-order80/src/main/java/com.atguigu.springcloud/controller/OrderFeignController.java
@@ -0,0 +1,25 @@
+package com.atguigu.springcloud.controller;
+
+import com.atguigu.springcloud.entities.CommonResult;
+import com.atguigu.springcloud.entities.Payment;
+import com.atguigu.springcloud.service.FeignPaymentService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
+
+@RestController
+@Slf4j
+public class OrderFeignController {
+
+    @Resource
+    private FeignPaymentService feignPaymentService;
+
+    @GetMapping(value = "/consumer/payment/get/{id}")
+    public CommonResult<Payment> getPaymentById(@PathVariable("id") Long id){
+        CommonResult<Payment> paymentById = feignPaymentService.getPaymentById(id);
+        return paymentById;
+    }
+}
